963e557c1bac9ce12e9080c87df6bad49f57f52a
João Emanuel <joemanuelalves1@gmail.com>
2025-06-04 08:55:47 -0300

    Ajustes no Round Robin

--- Escalonador.java
+++ Escalonador.java
@@ -19,24 +19,30 @@ public class Escalonador {
     }
 
     public static void RoundRobin(List<Processo> processos, int quantum) {
-        Queue<Processo> fila = new LinkedList<>(processos);
+        Queue<Processo> fila = new LinkedList<>();
+        fila.addAll(processos);
+
         int tempoAtual = 0;
 
         System.out.println("=== Execução Round Robin ===");
+
         while (!fila.isEmpty()) {
             Processo p = fila.poll();
 
-            if (p.tempoRestante > quantum) {
-                System.out.println("P" + p.id + " executa por " + quantum);
-                p.tempoRestante -= quantum;
-                tempoAtual += quantum;
-                fila.add(p);
-            } else {
-                System.out.println("P" + p.id + " executa por " + p.tempoRestante + " e finaliza.");
-                tempoAtual += p.tempoRestante;
-                p.tempoEspera = Math.max(0, tempoAtual - p.tempoChegada - p.tempoExecucao);
-                p.tempoTurnaround = tempoAtual - p.tempoChegada;
-                p.tempoRestante = 0;
+            if (p.tempoRestante > 0) {
+                if (p.tempoRestante > quantum) {
+                    System.out.println("P" + p.id + " executa por " + quantum);
+                    tempoAtual += quantum;
+                    p.tempoRestante -= quantum;
+                    fila.add(p);
+                } else {
+                    System.out.println("P" + p.id + " executa por " + p.tempoRestante + " e finaliza.");
+                    tempoAtual += p.tempoRestante;
+                    p.tempoRestante = 0;
+
+                    p.tempoTurnaround = tempoAtual - p.tempoChegada;
+                    p.tempoEspera = p.tempoTurnaround - p.tempoExecucao;
+                }
             }
         }
     }
